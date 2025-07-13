dd4b54f32f28023708e53405e82601eb2ff559bb
Vipul Verma <56539924+vipulverma2806@users.noreply.github.com>
2025-07-14 01:24:15 +0530

    setup

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser"
+dotenv.config()
+import cors from "cors"
+import userRouter from "./routes/user.routes.js"
+import messageRouter from "./routes/message.routes.js"
+import { app, server } from "./socket/socket.js"
+
+const port=process.env.PORT || 5000
+
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+app.use("/api/message",messageRouter)
+
+
+
+server.listen(port,()=>{
+    connectDb()
+    console.log("server started")
+})
